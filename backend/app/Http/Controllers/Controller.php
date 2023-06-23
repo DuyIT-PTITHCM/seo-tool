5d521e5acbff8de13bb1cd1088ff3d1930b83776
[REVU Anh Duy] <anhduy@revu.com>
2023-06-23 11:03:32 +0700

    add core response

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Libraries\Core;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    protected $core;
     use AuthorizesRequests, ValidatesRequests;
+
+    public function __construct()
+    {
+        $this->core = new Core();
+    }
 }
